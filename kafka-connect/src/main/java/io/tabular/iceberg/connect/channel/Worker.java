9ebd1a79e3bf2565b9a2047e046fcaaa06dd4643
wrehman-skap <wasi@streamkap.com>
2025-07-29 14:01:01 +0100

    Revert "Removing lower casing of dynamic table name."
    This reverts commit be02a33b3d71c2966cbb7dd6255854d0ae355b72.

--- kafka-connect/src/main/java/io/tabular/iceberg/connect/channel/Worker.java
+++ kafka-connect/src/main/java/io/tabular/iceberg/connect/channel/Worker.java
@@ -141,7 +141,7 @@ class Worker implements Writer, AutoCloseable {
 
     String routeValue = extractRouteValue(record, routeField);
     if (routeValue != null) {
-      String tableName = routeValue; // removing toLowerCase();
+      String tableName = routeValue.toLowerCase();
       writerForTable(tableName, record, true).write(record);
     }
   }
